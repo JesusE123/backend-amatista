f5e686c38ebda8f8a717ae8ae145b37dc3e57782
Jesus Sanchez <Jesusan0110@gmail.com>
2024-09-26 19:57:10 -0300

    add response codigo report

--- Controllers/PdfGeneratorController.cs
+++ Controllers/PdfGeneratorController.cs
@@ -16,11 +16,11 @@ namespace BackendAmatista.Controllers
         [HttpGet]
         public async Task<IActionResult> createReport(int id_sale)
         {
-            // Consulta para obtener la venta específica con el ID_SALE proporcionado
+            
             var report = await (from s in _dbamatistaContext.Sales
                                 join sd in _dbamatistaContext.SaleDetails on s.IdSale equals sd.IdSale
                                 join p in _dbamatistaContext.Products on sd.IdProduct equals p.IdProduct
-                                where s.IdSale == id_sale // Filtramos por ID_Sale
+                                where s.IdSale == id_sale 
                                 select new
                                 {
                                     ID_Sale = s.IdSale,
@@ -31,9 +31,10 @@ namespace BackendAmatista.Controllers
                                     InvoiceNumber = s.InvoiceNumber,
                                     Total = s.Total,
                                     Notes = s.Notes,
+                                    Codigo = p.Item
                                 }).ToListAsync();
 
-            // Verificar si se encontraron ventas para el ID_SALE proporcionado
+            
             if (report == null || !report.Any())
             {
                 return NotFound($"No se encontraron ventas con el ID_SALE: {id_sale}");
